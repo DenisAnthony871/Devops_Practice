d09abe1bc330ceced9d0b304c745306d9997448b
DenisAnthony871 <shadowfall0506@gmail.com>
2025-09-27 10:51:24 +0530

    feat: Add sysinfo.sh script

---
+++ scripts/sysinfo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#!/bin/bash
+
+echo "--- System Information ---"
+
+echo "Current User: $(whoami)"
+echo "Current Date: $(date)"
+
+echo ""
+echo "Disk Usage:"
+df -h
+
+echo "--------------------------"
